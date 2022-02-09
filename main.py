240a53872db0836d12938a00e0dea2c5cbb395a6
roy <royvanlierop92@gmail.com>
2022-02-09 20:53:15 +0100

    GPIO test en remote interpreter

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from gpiozero import LED
+from time import sleep     # Import the sleep function from the time module
 
+led = LED(14)
 
 if __name__ == '__main__':
-    print(f'Begin van HeeftRoyDeKoffieKlaar.nl')
+    while True:  # Run forever
+        led.on()
+        sleep(1)
+        led.off()
+        sleep(1)
