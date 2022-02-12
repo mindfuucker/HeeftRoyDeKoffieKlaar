0d3edbcab4a31c46e47b5704dc0263ef26d4ed4a
roy <royvanlierop92@gmail.com>
2022-02-12 15:35:38 +0100

    Multiple serial configs in ini file

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from configparser import ConfigParser
+from datetime import datetime, timezone
 import multiprocessing
 import serial
 import time
 import logging
+
 import threading
 import queue
 
@@ -15,30 +17,43 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 class Measurement:
-    def __init__(self, value):
-        self.val = value
+    def __init__(self, meas_name, val):
+        self.timestamp = self.createtimestamp()
+        self.value = self.sanitize(val)
+        self.name = meas_name
 
     def validate(self):
         retval = True
         # Serial connections can inject some false data
-        for char in self.val:
+        for char in self.value:
             if not char.isdigit():
                 retval = False
         # 12bit value, so 4095 max
-        if self.val < 0 or self.val > 4095:
+        if self.value < 0 or self.value > 4095:
             retval = False
         return retval
 
+    @staticmethod
+    def sanitize(inputbytestring):
+        return inputbytestring.decode("utf-8").strip()
 
-def serialreadforever(serinstance):
+    @staticmethod
+    def createtimestamp():
+        return datetime.utcnow().isoformat("T") + "Z"
+
+    def print(self):
+        return f'{self.name}, {self.timestamp}: {self.value}'
+
+
+def serialreadforever(measurementname, serinstance):
     try:
         while True:
             line = serinstance.readline()
-            line = line.decode("utf-8").strip()
-            logging.debug(line)
-    # The connection can be a bit flaky
+            m = Measurement(measurementname, line)
+            logging.debug(m.print())
+    # The connection can be a bit flaky, Restart
     except serial.SerialException:
-        serialreadforever(serinstance)
+        serialreadforever(serinstance, measurementname)
 
 
 def setup():
@@ -46,17 +61,20 @@ def setup():
     config = ConfigParser()
     config.read('config.ini')
 
-    serialports.append(serial.Serial(port=config['Serial']['Port'],
-                                     baudrate=config['Serial']['Baud']))
+    serialsectionnames = [s for s in config.sections() if s.startswith('Serial')]
+    for sname in serialsectionnames:
+        serialconfig = config[sname]
+        serialports.append([serialconfig['Name'], serial.Serial(port=serialconfig['Port'],
+                                                                baudrate=serialconfig['Baud'])])
 
 
 if __name__ == "__main__":
     setup()
-    print('Version 8')
+    print('Version 10')
     # Check if SerialPort is outputting data
-    if serialports[0].readline():
-        for ser in serialports:
-            process = multiprocessing.Process(target=serialreadforever, args=(ser,))
+    if serialports[0][1].readline():
+        for name, ser in serialports:
+            process = multiprocessing.Process(target=serialreadforever, args=(name, ser))
             process.start()
             processpool.append(process)
 
